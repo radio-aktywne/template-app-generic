086ce9153f9138b283c5c83f5f36f6ec9f417253
Sebastian Pietras <me@spietras.dev>
2023-07-27 18:33:12 +0200

    Performed various cleanups (#4)

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -11,10 +11,16 @@ def data() -> dict[str, str]:
     """Return a dictionary with the data to be used in the template."""
 
     return {
+        "accountname": "radio-aktywne",
         "appname": "foo",
-        "description": "Generic app",
+        "description": "Example app",
+        "reponame": "app-foo",
+        "repourl": "https://github.com/radio-aktywne/app-foo",
+        "envprefix": "FOO",
         "docs": "true",
+        "docsurl": "https://radio-aktywne.github.io/app-foo",
         "releases": "false",
+        "registry": "false",
     }
 
 
